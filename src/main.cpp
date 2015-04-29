97c363fc7f0110684faee768dd9006f3c6022b6c
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2015-04-29 14:46:13 +0200

    Add the barebones logic needed to import a scene into Assimp and start exporting it

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 
 #include <iostream>
 
+extern Assimp::Exporter::ExportFormatEntry Assimp2XML3D_desc;
+
 int invalidUsageExit() {
 	std::cout << "usage: assimp2xml3d [FLAGS] inputFile [outputFile]" << std::endl;
 	return -1;
@@ -21,9 +23,25 @@ int invalidUsageExit() {
 
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
+	if (argc < 3) {
 		return invalidUsageExit();
 	}
 
+	const char* input = argv[1], *output = argv[2];
 
+	Assimp::Importer importer;
+	importer.SetPropertyBool(AI_CONFIG_PP_FD_REMOVE, true);
+
+	const aiScene* const scene = importer.ReadFile(input, aiProcessPreset_TargetRealtime_MaxQuality);
+
+	if (!scene) {
+		std::cerr << "Assimp: Could not read file " << input << std::endl;
+		return -1;
+	}
+
+	std::cout << "Assimp read file successfully";
+
+	Assimp::Exporter exporter;
+	exporter.RegisterExporter(Assimp2XML3D_desc);
+	exporter.Export(scene, "xml", output);
 }
